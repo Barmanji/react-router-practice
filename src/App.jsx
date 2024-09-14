7d860dc505abc3371bb49cc419de216799187d48
Barmanji <barmanjiaj@gmail.com>
2024-09-14 17:17:02 +0530

    first commit, just made react file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-slate-600'>Check 1,2,3...</h1>
+    </>
+  )
+}
+
+export default App
