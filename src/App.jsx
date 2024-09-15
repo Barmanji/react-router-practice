ad8d83ac30fc3146c61a04d20d39d731695463e5
Barmanji <barmanjiaj@gmail.com>
2024-09-15 18:05:40 +0530

    completed Hitesh sir HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-slate-600'>Check 1,2,3...</h1>
+
+
     </>
   )
 }
